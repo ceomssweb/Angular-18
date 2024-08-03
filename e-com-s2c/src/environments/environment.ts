59512c98c819dbbb23bdb82a47bd9bb2959e181b
ceomssweb <ceomssweb@gmail.com>
2024-08-03 15:37:56 +0530

    added firebase setup

---
+++ e-com-s2c/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyC_X9DD_B8nzTou0W23gLlLKaSlFe7uhDA",
+        authDomain: "e-com-s2c.firebaseapp.com",
+        projectId: "e-com-s2c",
+        storageBucket: "e-com-s2c.appspot.com",
+        messagingSenderId: "573966655805",
+        appId: "1:573966655805:web:b1422578ffe380195da554",
+        measurementId: "G-8WNM2YFJ08"
+    },
+    backendUrl: 'http://localhost:3000'
+  };
--- e-com-s2c/src/main.ts
+++ e-com-s2c/src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = environment.firebase;
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  if(environment.production){
+    enableProdMode()
+  }
+
